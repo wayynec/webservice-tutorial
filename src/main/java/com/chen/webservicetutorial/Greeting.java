f9d3ba2db66bc2bc146af9c522c102069be9acef
weichen3 <weichen3@uvic.ca>
2022-05-11 15:28:24 -0700

    A simple web service demo

---
+++ src/main/java/com/chen/webservicetutorial/Greeting.java
@@ -0,0 +1,19 @@
+package com.chen.webservicetutorial;
+
+public class Greeting {
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
