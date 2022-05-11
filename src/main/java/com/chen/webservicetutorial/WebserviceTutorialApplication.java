b6331d5cd0742c064b7935906c2e14573223daa2
weichen3 <weichen3@uvic.ca>
2022-05-11 11:41:23 -0700

    First commit

---
+++ src/main/java/com/chen/webservicetutorial/WebserviceTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.chen.webservicetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chen/webservicetutorial/WebserviceTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.webservicetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
